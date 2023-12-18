0db0c85284cf8196dc7a9e8fe9c6d6f0fbcb342b
Anshuman <107368860+anshuman008@users.noreply.github.com>
2023-12-19 02:31:54 +0530

    ep-2 completed

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-
-    
-    
+import React from "react";
+import  ReactDOM  from "react-dom/client";
     
     const parrent = React.createElement(
     "div",
     {id: "parrent"},
     React.createElement("div",{id:"child"},
-   [React.createElement("h1",{},"Im a h1 tag"),React.createElement("h1",{},"Im a h2 tag")]
+   [React.createElement("h1",{},"Im div not A tag"),React.createElement("h1",{},"Im a h2 tag")]
     ));
 
 
