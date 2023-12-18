20c99b87a8cda719185a801cca00089aa6ad85e5
Anshuman <107368860+anshuman008@users.noreply.github.com>
2023-12-19 04:52:00 +0530

    ep-3 completed

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
     
-    const parrent = React.createElement(
-    "div",
-    {id: "parrent"},
-    React.createElement("div",{id:"child"},
-   [React.createElement("h1",{},"Im div not A tag"),React.createElement("h1",{},"Im a h2 tag")]
-    ));
+//     const parrent = React.createElement(
+//     "div",
+//     {id: "parrent"},
+//     React.createElement("div",{id:"child"},
+//    [React.createElement("h1",{},"Im div not A tag"),React.createElement("h1",{},"Im a h2 tag")]
+//     ));
 
 
+const Conatainer = () => {
+   return (
+    <div>
+        <h2>im form  Container</h2>
+    </div>
+   )
+}
+
+// functional components
+
+const HeadingComp = () =>  {
+    return (<div>
+        hello world!!
+        {Conatainer()}
+        <Conatainer/>
+    </div>)
+}
 
     const root = ReactDOM.createRoot(document.getElementById("root"));
 
-    root.render(parrent);
+    root.render(<HeadingComp/>);
